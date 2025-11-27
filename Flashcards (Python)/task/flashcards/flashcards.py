9c1db0e4c58ab704795c496e0b0ecccd57585388
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-27 22:43:29 +0100

    Refactor flashcards logic to use a Flashcard dataclass and improve user input handling

--- Flashcards (Python)/task/flashcards/flashcards.py
+++ Flashcards (Python)/task/flashcards/flashcards.py
@@ -1,15 +1,36 @@
 """Flashcards application module."""
 
+from dataclasses import dataclass
+
+
+@dataclass
+class Flashcard:
+    term: str
+    definition: str
+
 
 def main():
-    term = input()
-    definition = input()
-    answer = input()
+    cards = []
+    print("Input the number of cards:")
+    n_cards = int(input())
 
-    if answer == definition:
-        print("Your answer is right!")
-    else:
-        print("Your answer is wrong...")
+    for i in range(1, n_cards + 1):
+        print(f"The term for card #{i}:")
+        term = input()
+        print(f"The definition for card #{i}:")
+        definition = input()
+        cards.append(Flashcard(term, definition))
+
+    for c in cards:
+        term = c.term
+        definition = c.definition
+        print(f'Print the definition of "{term}":')
+        answer = input()
+
+        if answer == definition:
+            print("Correct!")
+        else:
+            print(f"Wrong. The right answer is \"{definition}\".")
 
 
 if __name__ == "__main__":
