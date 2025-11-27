04ef3bd371df9d4a28cc1a0e5ce251423276a155
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-27 22:33:16 +0100

    Implement basic flashcards application structure

--- Flashcards (Python)/task/flashcards/flashcards.py
+++ Flashcards (Python)/task/flashcards/flashcards.py
@@ -1,1 +1,14 @@
-# Write your code here
+"""Flashcards application module."""
+
+
+def main():
+    print("Card:")
+    term = input()
+    print(term)
+    print("Definition:")
+    definition = input()
+    print(definition)
+
+
+if __name__ == "__main__":
+    main()
